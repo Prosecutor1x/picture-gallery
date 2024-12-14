36b704c856371bb0c148bf4af95a7d229578d9c1
Krishnarjun Nanda <krishnarjunnanda@gmail.com>
2024-12-14 15:18:15 +0530

    Update footer.tsx

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full text-white">
       <h1 className="bg-gray-900 py-2 text-center">
-        All Rights Reserved @Ed Churan
+        All Rights Reserved @Drason Smtih and RJ sharma
       </h1>
     </div>
   );
